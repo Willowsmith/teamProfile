f5696978b46fd4adcdd7ca34e242dfc23e3728f5
Willowsmith <willowrosedennis@gmail.com>
2021-10-22 13:18:44 -0700

    Fixed the massive issues in the index.js file. It appears to function now.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # teamProfile
+
+
+## Creating a clean and responsive website for a professional portfolio.
+
+-My motivation was to create code that allows a user to launch a website showing their team using the console to input the data.
+-I built this project so that I could learn how to create team profile website utilizing express and node as well as writing code to pass tests.
+-The problem that I solved were the display of data, making sure the tests passed, and that the information was correctly displayed.
+-I learned about TDD, express, and node.
+
+# Installation
+
+There are no steps required to install this project, simply launch the website in your preferred web browser and select the generate password button. You can locate the website at: [Website:](https://github.com/Willowsmith/teamProfile)
+
+# Usage
+
+Launch the website and utilize the functions found therein.
+
+# Credits
+
+Melyssa Dennis
--- index.js
+++ index.js
@@ -1,49 +1,20 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+
+
 const html = require("./util/generateHtml");
-const Employee = require("./lib/Employee");
+
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const employees = [];
+
+const empArr = [];
+const manArr = [];
+const engArr = [];
+const intArr = [];
 
 
-
-function newEmp() {
-    inquirer.prompt({
-        name: "name",
-        message: "What is your name?",
-        type: "input"
-    }).then(({ name }) => {
-        console.log(name);
-        const me = new Employee(name);
-        console.log(employees);
-        askEmail();
-    })
-}
-
-function askEmail() {
-    inquirer.prompt({
-        name: "email",
-        message: "What is your email address?",
-        type: "input"
-    }).then(({ email }) => {
-        console.log(email);
-        const em = new Employee(email);
-        askId();
-    })
-}
-
-function askId() {
-    inquirer.prompt({
-        name: "id",
-        message: "What is your employee id?",
-        type: "input"
-    }).then(({ id }) => {
-        console.log(id);
-        const id = new Employee(id);
-        askQuestion();
-    })
-}
 
 function askQuestion() {
     inquirer.prompt([
@@ -66,40 +37,94 @@ function askQuestion() {
                 console.log("intern!")
                 intern();
                 break;
+            default:
+                console.log('Go Team!');
+                fs.writeFile(`Team.html`, html(empArr), (err) =>
+                err ? console.error(err) : console.log('HTML Created!')
+                );
         }
     })
 }
 
 function engineer() {
-    inquirer.prompt({
-        name: "github",
-        message: "What is your github address?",
-        type: "input"
-    }).then(({ github }) => {
-        console.log(github);
-        const git = new Engineer(git);
+    inquirer.prompt([
+        {
+        message:"What is your name?",
+        type: "input",
+        name:"engName",
+        },{
+        message:"What is the your employee ID?",
+        type: "input",
+        name:"engId"
+        },{
+        message:"What is the your E-Mail?",
+        type: "input",
+        name:"engEmail"
+        },{
+        message:"What is the your GitHub Username?",
+        type: "input",
+        name:"engGitHub"
+        }
+    ]).then(engAnswers => {
+        const myEngineer = new Engineer(engAnswers.engName, engAnswers.engId, engAnswers.engEmail, engAnswers.engGitHub);
+        empArr.push(myEngineer);
+        engArr.push(myEngineer);
+        askEngInt();
     })
+
 }
 
 function manager() {
-    inquirer.prompt({
-        name: "officeNumber",
-        message: "What is your office number?",
-        type: "input"
-    }).then(({ officeNumber }) => {
-        console.log(officeNumber);
-        const on = new Manager(on);
+    inquirer.prompt([
+        {
+        message:"What is the your Name?",
+        type: "input",
+        name:"manName",
+        },{
+        message:"What is the your employee ID?",
+        type: "input",
+        name:"manId"
+        },{
+        message:"What is your E-Mail?",
+        type: "input",
+        name:"manEmail"
+        },{
+        message:"What is the your Office Number?",
+        type: "input",
+        name:"manOffice"
+        }
+    ]).then(manAnswers => {
+        const myManager = new Manager(manAnswers.manName, manAnswers.manId, manAnswers.manEmail, manAnswers.manOffice);
+        empArr.push(myManager);
+        manArr.push(myManager);
+        askEngInt();
     })
 }
 
 function intern() {
-    inquirer.prompt({
-        name: "school",
-        message: "What school did you go to?",
-        type: "input"
-    }).then(({ school }) => {
-        console.log(school);
-        const school = new Intern(school);
+    inquirer.prompt([
+        {
+        message:"What is tyour Name?",
+        type: "input",
+        name:"intName",
+        },{
+        message:"What is your Employee ID?",
+        type: "input",
+        name:"intId"
+        },{
+        message:"What is your E-Mail?",
+        type: "input",
+        name:"intEmail"
+        },{
+        message:"What is your School?",
+        type: "input",
+        name:"intSchool"
+        }
+    ]).then(intAnswers => {
+        const myIntern = new Intern(intAnswers.intName, intAnswers.intId, intAnswers.intEmail, intAnswers.intSchool);
+        empArr.push(myIntern);
+        intArr.push(myIntern);
+        askEngInt();
     })
 }
 
